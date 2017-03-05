6b0284a019e0fdaed76a2f08355ca3f4a85aae70
Aron Cederholm <aron.cederholm@gmail.com>
2017-03-05 20:10:02 +0100

    Add fully working tab switching to the rofi modi

--- plasmiumrofimodi/plasmiumrofimodi.cpp
+++ plasmiumrofimodi/plasmiumrofimodi.cpp
@@ -13,6 +13,7 @@ int main(int argc, char **argv)
     QCoreApplication app(argc, argv);
 
     qDBusRegisterMetaType<QStringMap>();
+    qDBusRegisterMetaType<QStringMapMap>();
 
     QDBusConnection bus = QDBusConnection::sessionBus();
     if (!bus.isConnected()) {
@@ -24,30 +25,63 @@ int main(int argc, char **argv)
 
     QDBusInterface interface(SERVICE_NAME, "/", "", bus);
     if (interface.isValid()) {
-        QDBusReply<QStringMap> reply = interface.call("getTopSites");
-        QStringList sites;
-        if (reply.isValid()) {
-            QStringMap map = reply.value();
+        QString search;
+        if (argc > 1) {
+            search = QString(argv[1]);
+        }
+        QStringList items;
+
+        // Get list of all open tabs
+        QDBusReply<QStringMapMap> tabsReply = interface.call("getTabs");
+        if (tabsReply.isValid()) {
+            QStringMapMap windowMap = tabsReply.value();
+            for (QStringMapMap::const_iterator it = windowMap.constBegin(), end = windowMap.constEnd(); it != end; ++it) {
+                QStringMap tabMap = *it;
+                QString current("tab: %1 (%2)");
+                current = current.arg(tabMap["title"], tabMap["uri"]);
+                // if execute statement matches, switch to this tab
+                if (argc > 1 && current == search) {
+                    bool ok;
+                    interface.call("highlightTab", tabMap["windowId"].toInt(&ok, 10), tabMap["tabIndex"].toInt(&ok, 10));
+                    return 0;
+                }
+                items << current;
+            }
+        } else {
+            std::cerr << "Got an invalid reply from plasmium." << tabsReply.error().message().toStdString() << std::endl;
+            return 1;
+        }
+
+        // Get list of all top sites
+        QDBusReply<QStringMap> topsitesReply = interface.call("getTopSites");
+        if (topsitesReply.isValid()) {
+            QStringMap map = topsitesReply.value();
             for (QStringMap::const_iterator it = map.constBegin(), end = map.constEnd(); it != end; ++it) {
                 QString current("%1 (%2)");
                 current = current.arg(it.value(), it.key());
-                if (argc > 1 && current == QString(argv[1])) {
+                // if execute statement matches, launch this top site
+                if (argc > 1 && current == search) {
                     interface.call("newTab", it.key());
                     return 0;
                 }
-                sites << current;
+                items << current;
             }
-            if (argc > 1) {
-                interface.call("newTab", QString(argv[1]));
-                return 0;
-            }
-            for (QStringList::const_iterator it = sites.constBegin(), end = sites.constEnd(); it != end; ++it) {
-                std::cout << (*it).toStdString() << std::endl;
-            }
+        } else {
+            std::cerr << "Got an invalid reply from plasmium." << std::endl;
+            return 1;
+        }
+
+        // Run new tab if no hit was found
+        if (argc > 1) {
+            interface.call("newTab", search);
             return 0;
         }
-        std::cerr << "Got an invalid reply from plasmium." << std::endl;
-        return 1;
+
+        // If no execute statement was supplied, print available entries.
+        for (QStringList::const_iterator it = items.constBegin(), end = items.constEnd(); it != end; ++it) {
+            std::cout << (*it).toStdString() << std::endl;
+        }
+        return 0;
     }
 
     std::cerr << "Couldn't connect to chrome. Please make sure plasmium is installed and activated in Chrome." << std::endl;
